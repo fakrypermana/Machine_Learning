d29c018dead7fd07939020c6266c423a73662ae3
fakrypermana <fakrypermana@gmail.com>
2019-02-24 21:22:25 +0700

    first commit

---
+++ Naive/venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!D:\perkuliahan\mata_kuliah\semester_6\Machine_Learning\Naive\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
